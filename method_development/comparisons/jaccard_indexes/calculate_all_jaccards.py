f036e9fb2df01655387d1e30521842d29cbe05b6
AnnaSHessevik <103359908+AnnaSHessevik@users.noreply.github.com>
2022-04-10 13:24:37 +0200

    Add files via upload

---
+++ method_development/comparisons/jaccard_indexes/calculate_all_jaccards.py
@@ -0,0 +1,67 @@
+#Calculate jaccard indexes between "correct" correlations between gene pairs and estimated correlations
+
+python3 remove_minus_and_selfcorr.py spearman_not_sun_1000genes.txt 2 copy_spearman_not_sun_1000genes.txt #remove minus signs and self-correlations
+sort -k 3gr copy_spearman_not_sun_1000genes.txt > sorted_numerically_spearman_not_sun_1000genes.txt #sort numerically
+rm copy_spearman_not_sun_1000genes.txt #remove unnecessary copy
+
+i=0 #counter
+
+for file in combined_spearman_not_sun*; #Compare "correct" values with estimated values
+do
+    ((i++))
+    echo "Loop number $i: $file" #follow the loop
+    python3 remove_minus_and_selfcorr.py $file -1 "copy_$file" #remove minus in arithmetric average
+    python3 remove_minus_and_selfcorr.py "copy_$file" -2 "no_minus_$file" #remove minus in backtransformed average
+    rm copy_$file #remove redundant file
+    sort -k 3gr no_minus_$file > sorted_fisher_$file #sort file numerically by backtransformed averages
+    sort -k 4gr no_minus_$file > sorted_arithmetic_$file #sort file numerically by arithmetic averages
+    rm no_minus_$file #remove redundant file 
+
+    #Calculate jaccard indexes at n number of gene pairs
+    for j in 1 10 100 1000 10000 100000;
+    do
+
+	for k in 1 3 5 8;
+	do
+	    n=$[$j*$k]
+	    echo "Number of gene pairs = $n"
+	    python3 calculate_jaccard.py sorted_numerically_spearman_not_sun_1000genes.txt sorted_fisher_$file $n jaccard_fisher #Calculate jaccard index for correct vs. fisher values	    
+	    python3 calculate_jaccard.py sorted_numerically_spearman_not_sun_1000genes.txt sorted_arithmetic_$file $n jaccard_arithmetic #Caluclate jaccard index for correct vs. arritmetric values
+	    
+	done
+    done
+
+    n=999000 #maximal number of gene pairs (excluding self-correlations)
+    echo "Number of gene pairs = $n"
+    python3 calculate_jaccard.py sorted_numerically_spearman_not_sun_1000genes.txt sorted_fisher_$file $n jaccard_fisher #Calculate jaccard index for correct vs. fisher values
+    python3 calculate_jaccard.py sorted_numerically_spearman_not_sun_1000genes.txt sorted_arithmetic_$file $n jaccard_arithmetic #Caluclate jaccard index for correct vs. arithmetic values
+    	
+    #Remove redundant files
+    rm sorted_fisher_$file
+    rm sorted_arithmetic_$file
+done
+
+#Remove redundant file
+rm sorted_numerically_spearman_not_sun_1000genes.txt
+
+
+
+#Calculate means and standard deviations
+echo "Calculation of mean and standard deviatons"
+i=0
+for j in 1 10 100 1000 10000 100000;
+do
+
+    for k in 1 3 5 8;
+    do
+	n=$[$j*$k]
+	echo "Number of gene pairs = $n"
+	python3 jaccard_mean_std.py jaccard_fisher$n.txt $n summary_fisher_jaccards.txt #calculate mean and standard deviation for jaccard indexes based on fisher transformed correlations
+	python3 jaccard_mean_std.py jaccard_arithmetic$n.txt $n summary_arithmetic_jaccards.txt #calucalte mean and standad deviation for jaccard indexes based on arithmetic averages of correlations
+    done
+done
+
+n=999000 #maximal number of gene pairs (excluding self-correlations)
+echo "Number of gene pairs = $n"
+python3 jaccard_mean_std.py jaccard_fisher$n.txt $n summary_fisher_jaccards.txt #calculate mean and standard deviation for jaccard indexes based on fisher transformed correlations
+python3 jaccard_mean_std.py jaccard_arithmetic$n.txt $n summary_arithmetic_jaccards.txt #calucalte mean and standad deviation for jaccard indexes based on arithmetic averages of correlations
---
+++ method_development/comparisons/jaccard_indexes/calculate_all_subsample_jaccards.sh
@@ -0,0 +1,80 @@
+#Calculate jaccard indexes between "correct" correlations between gene pairs and subsample correlations
+
+python3 remove_minus_and_selfcorr.py spearman_not_sun_1000genes.txt 2 copy_spearman_not_sun_1000genes.txt #remove minus signs and self-correlations
+sort -k 3gr copy_spearman_not_sun_1000genes.txt > numerically_sorted_spearman_not_sun_1000genes.txt #sort numerically
+rm copy_spearman_not_sun_1000genes.txt #remove unnecessary copy
+
+i=0 #counter
+
+for file in spearman_subsample*; #Compare "correct" values with estimated values
+do
+    ((i++))
+    echo "Loop number $i: $file" #follow the loop
+    python3 remove_minus_and_selfcorr.py $file 2 "copy_$file" #remove self-correlations and minus in correlations
+    
+    sort -k 3gr copy_$file > numerically_sorted_$file #sort file numerically by backtransformed averages
+    rm copy_$file #remove redundant file
+    
+    sample_size=$(python3 get_sample_size.py $file) #find correct sample size
+    if (($sample_size > 9)) && (($sample_size < 20));
+    then
+	size_range="10-19"
+    elif (($sample_size > 19)) && (($sample_size < 30));
+    then
+	size_range="20-29"
+    elif (($sample_size > 29)) && (($sample_size < 40));
+    then
+	size_range="30-39"
+    elif (($sample_size > 29)) && (($sample_size < 50));
+    then
+	size_range="40-49"
+    else
+	size_range="not_included"
+    fi
+    
+    outfile="jaccard_subsample_size"$size_range"_"
+
+    for j in 1 10 100 1000 10000 100000;
+    do
+
+	for k in 1 3 5 8;
+	do
+	    n=$[$j*$k]
+	    echo "Number of gene pairs = $n"
+	    python3 calculate_jaccard.py numerically_sorted_spearman_not_sun_1000genes.txt numerically_sorted_$file $n $outfile #Calculate jaccard index for correct vs. subsample values
+	done
+    done
+    n=999000 #maximal number of gene pairs (excluding self-correlations)
+    echo "Number of gene pairs = $n"
+    python3 calculate_jaccard.py numerically_sorted_spearman_not_sun_1000genes.txt numerically_sorted_$file $n $outfile #Calculate jaccard index for correct vs. subsample values
+    
+    rm numerically_sorted_$file
+    
+done
+
+rm numerically_sorted_spearman_not_sun_1000genes.txt
+
+
+#Calculate means and standard deviations
+echo "Calculation of mean and standard deviatons"
+for j in 1 10 100 1000 10000 100000;
+do
+    for k in 1 3 5 8;
+    do
+	n=$[$j*$k]
+	for size_range in "10-19" "20-29" "30-39" "40-49";
+	do
+	    echo "Sample size $size_range, n=$n"
+	    python3 jaccard_mean_std.py "jaccard_subsample_size"$size_range"_"$n".txt" $n "summary_jaccards_subsample_size"$size_range".txt" #calculate mean and standard deviation for jaccard indexes
+	done
+    done
+done
+
+n=999000 #maximal number of gene pairs (excluding self-correlations)
+for size_range in "10-19" "20-29" "30-39" "40-49";
+do
+    echo "Sample size $size_range, n=$n"
+    python3 jaccard_mean_std.py "jaccard_subsample_size"$size_range"_"$n".txt" $n "summary_jaccards_subsample_size"$size_range".txt" #calculate mean and standard deviation for jaccard indexes
+done
+
+
