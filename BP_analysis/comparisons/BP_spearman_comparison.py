d67ee7d75a2c89fa3435592a1c3940789b7832d7
AnnaSHessevik <103359908+AnnaSHessevik@users.noreply.github.com>
2022-04-10 15:38:55 +0200

    Add files via upload

---
+++ BP_analysis/comparisons/BP_spearman_comparison.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 13:03:08 2022
+
+
+@author: annas
+This script is a modified version of subsample_correlation_of_correlations.
+It calculates the Spearman rank correlation between the correlation coefficients
+based on Fisher's Z transformation and the untransformed mean.
+
+The arguments correspond to:
+    1) Reference file (to which the other file will be compared, here: fisher)
+    2) The input file, a file with Spearman rank correlations (here: arithmetic average)
+    3) Y if it is wishful to make a plot
+    4) "scatter" or "heatmap" depending on the wanted plot type
+    
+Remember to change the colorbar range of the heatmap
+"""
+
+import sys
+from scipy import stats
+#import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+def calculate_correlation(fisher_corr):
+    if len(sys.argv) != 5:
+        return 0,0
+    
+    try:
+        #Input argument is the name of a file with estimated correlations
+        test_corr = open(sys.argv[2])
+        #Make lists with correlations
+        fisher_corr_list = []
+        test_corr_list = []
+        
+        for line1 in fisher_corr:
+            line_split1 = line1.rstrip().split('\t')
+            if line_split1[0] == line_split1[1]: #Do not include self-correlations
+                next(test_corr)
+                continue
+            fisher_corr_list.append(float(line_split1[2]))            
+            
+            line2 = test_corr.readline()
+            line_split2 = line2.rstrip().split('\t')
+            test_corr_list.append(float(line_split2[2]))
+        
+        test_corr.close()
+                    
+        spearman = stats.spearmanr(fisher_corr_list, test_corr_list)
+        
+        if sys.argv[3] == "Y":
+            #Scatter plot
+            if sys.argv[4] == "scatter":
+                plt.figure(0)
+                plt.scatter(test_corr_list,fisher_corr_list, c="skyblue")
+                plt.xlim([-1, 1])
+                plt.ylim([-1, 1])
+                plt.xlabel("Estimated combined Spearman rank correlation coefficient \nusing weighted untransformed values", fontweight='bold')
+                plt.ylabel("Estimated combined Spearman rank correlation coefficient \nusing Fisher's Z transformed values", fontweight='bold')
+                plt.show()
+                plt.savefig(sys.argv[2] +"correlation_of_correlations_scatter"+".png")
+            #Heatmap
+            elif sys.argv[4] == "heatmap":
+                plt.figure(1)            
+                plt.hist2d(test_corr_list, fisher_corr_list, bins=100, norm=mcolors.LogNorm(), cmap='Reds', range=[[-1, 1], [-1, 1]])
+                plt.clim(1,350000)
+                cb = plt.colorbar()
+                cb.set_label('Number of entries')
+                plt.xlabel("Estimated combined Spearman rank correlation \ncoefficient using weighted untransformed values", fontweight='bold')
+                plt.ylabel("Estimated combined Spearman rank correlation \ncoefficient using Fisher's Z transformed values", fontweight='bold')
+                plt.text(-0.9, 0.8, r'$r_s$ = $%.4f$' % (spearman[0]), fontsize=12)
+                #plt.show()
+                plt.plot([-1, 1], [-1,1],linestyle='dashed', color = 'black')
+                plt.savefig(sys.argv[2] +"correlation_of_correlations_heatmap.png", dpi=500, bbox_inches='tight')
+        return spearman
+    except:
+        return 0
+    
+if __name__ == '__main__':
+    fisher_corr = open(sys.argv[1])
+    spearman = calculate_correlation(fisher_corr)
+    
+    if spearman != 0:
+        try: #Print correlation value
+            sys.stdout.write(str(spearman[0]))
+        except:
+            sys.stdout.write("Something failed\n")
+    else:
+        sys.stdout.write("Check your input arguments\n")
+               
+    fisher_corr.close()
+    
+
