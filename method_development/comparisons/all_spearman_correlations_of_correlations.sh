81c9a3d38dad10b67af050f21d9f6e3a415fd5ef
AnnaSHessevik <103359908+AnnaSHessevik@users.noreply.github.com>
2022-04-10 12:58:34 +0200

    Add files via upload

---
+++ method_development/comparisons/all_spearman_correlations_of_correlations.sh
@@ -0,0 +1,13 @@
+
+#Generate a new file with calculated Spearman rank correlations for all data sets (estimated vs. real values)
+
+set -o noclobber #prevent overwriting existing files
+echo -e "File number \tFisher vs real \tUntransformed average vs real" > comparison_correlation_of_correlations.txt
+
+for i in {1..100};
+do
+    echo "File$i"
+    what_to_write=$(python3 spearman_correlation_of_correlations.py $i N) #Find correlation of correlations
+    echo -e "$what_to_write" >> comparison_correlation_of_correlations.txt
+done
+    
