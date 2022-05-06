728d8e2d6eb162b9e4bae98fd4fe637283945b5a
AnnaSHessevik <103359908+AnnaSHessevik@users.noreply.github.com>
2022-05-06 11:16:03 +0200

    Update calculate_all_jaccards_BP.sh

--- BP_analysis/comparisons/jaccard_index_corrs/calculate_all_jaccards_BP.sh
+++ BP_analysis/comparisons/jaccard_index_corrs/calculate_all_jaccards_BP.sh
@@ -1,4 +1,4 @@
-#Calculate jaccard indexes between correlations based on Fisher's Z transformed values and weighted untransformed values
+#This script calculates Jaccard indexes between correlations based on Fisher's Z transformed values and weighted untransformed values
 
 #Sort file 1
 echo "Sort fisher file" #Follow progress 
